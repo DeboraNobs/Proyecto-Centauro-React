fd539513c7a7b7726bcdba85eec3fc49cba83f3a
Debora Vanesa Nobs <dnobs@centauro.net>
2025-05-08 10:06:47 +0200

    Skeleton loaders creados en Availability y Fleet

--- src/componentes/Fleet/Availability.tsx
+++ src/componentes/Fleet/Availability.tsx
@@ -3,85 +3,94 @@ import React, { useEffect, useState } from 'react'
 import { Car } from '../../types/types';
 import CarDetails from './CarDetails';
 import Button from '../Elements/Button';
+import SkeletonCard from '../Elements/SkeletonCard';
 
 const Availability = () => {
-  const [searchParams] = useSearchParams();
-  const fechaInicio = searchParams.get('fechainicio') ? new Date(searchParams.get('fechainicio')!) : new Date();
-  const fechaFin = searchParams.get('fechaFin') ? new Date(searchParams.get('fechaFin')!) : new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
-  const horarioRecogida = searchParams.get('horarioRecogida') || '10:00';
-  const horarioDevolucion = searchParams.get('horarioDevolucion') || '12:00';
-  const sucursalId = Number(searchParams.get('sucursalId') || '3');
-  const sucursalDevolucion = Number(searchParams.get('sucursalDevolucion') || '3');
+    const [searchParams] = useSearchParams();
+    const fechaInicio = searchParams.get('fechainicio') ? new Date(searchParams.get('fechainicio')!) : new Date();
+    const fechaFin = searchParams.get('fechaFin') ? new Date(searchParams.get('fechaFin')!) : new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
+    const horarioRecogida = searchParams.get('horarioRecogida') || '10:00';
+    const horarioDevolucion = searchParams.get('horarioDevolucion') || '12:00';
+    const sucursalId = Number(searchParams.get('sucursalId') || '3');
+    const sucursalDevolucion = Number(searchParams.get('sucursalDevolucion') || '3');
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const [coches, setCoches] = useState<Car[]>([]);
-  const navegarListado = () => { navigate('/'); }
+    const [coches, setCoches] = useState<Car[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (sucursalId) { 
-      fetch(`http://localhost:5038/api/coche/con-filtrado?sucursalId=${sucursalId}&fechainicio=${fechaInicio.toISOString()}&fechaFin=${fechaFin.toISOString()}&horarioRecogida=${horarioRecogida}&horarioDevolucion=${horarioDevolucion}`) 
-        .then((res) => res.json())
-        .then((data) => {
-            setCoches(data.$values || []);
-            console.log(data.$values);
-        });
-    }
-  }, [sucursalId]);
+    const navegarListado = () => { navigate('/'); }
+
+    useEffect(() => {
+        if (sucursalId) {
+            setIsLoading(true);
+            fetch(`http://localhost:5038/api/coche/con-filtrado?sucursalId=${sucursalId}&fechainicio=${fechaInicio.toISOString()}&fechaFin=${fechaFin.toISOString()}&horarioRecogida=${horarioRecogida}&horarioDevolucion=${horarioDevolucion}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setCoches(data.$values || []);
+                    console.log(data.$values);
+                    setIsLoading(false);
+                });
+        }
+    }, [sucursalId]);
 
 
     return (
         <div className='container'>
             <h2 className='mb-4'> Disponibilidad de coches </h2>
 
-            <div className='row'>
-                {coches.length > 0 ? (
-                    coches.map((coche) => (
-                        <div className="col-12 col-md-6 col-lg-4 mb-3"
-                            key={coche.id}
-                        >
-                        <CarDetails
-                                coche={coche}
-                                selectedSucursalId={sucursalId}
-                                sucursalDevolucion={sucursalDevolucion}  
-                                fechainicio={new Date(fechaInicio)}
-                                fechaFin={new Date(fechaFin)}
-                                selectedHorarioRecogida={horarioRecogida}
-                                selectedHorarioDevolucion={horarioDevolucion} 
-                        />
-                        </div>
-                    ))
-                ) : (
-                    <>
-                    <div className="d-flex justify-content-center mt-4 mb-4">
-                        <h4>Lo sentimos pero no tenemos coches disponibles para el <br />
-                            periodo seleccionado, prueba a elegir otras fechas.</h4>
-                    </div>
-
-                    <hr />
-
-                    <div className="d-flex justify-content-center">
-                     <Button
-                                        type="button"
-                                        style={{
-                                            color: "black",
-                                            padding: "7px",
-                                            width: '30%',
-                                            backgroundColor: "beige",
-                                            border: "solid BurlyWood 2px"
-                                        }}
-                                        onClick={navegarListado}
-                                        texto="Modificar búsqueda" />
-                     </div>
-
-                    <hr />
-    
-                        </>
-                )}
-
+           
+<div className='row'>
+    {isLoading ? (
+        <>
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+            <SkeletonCard />
+        </>
+    ) : coches.length > 0 ? (
+        coches.map((coche) => (
+            <div className="col-12 col-md-6 col-lg-4 mb-3" key={coche.id}>
+                <CarDetails
+                    coche={coche}
+                    selectedSucursalId={sucursalId}
+                    sucursalDevolucion={sucursalDevolucion}
+                    fechainicio={new Date(fechaInicio)}
+                    fechaFin={new Date(fechaFin)}
+                    selectedHorarioRecogida={horarioRecogida}
+                    selectedHorarioDevolucion={horarioDevolucion}
+                />
             </div>
+        ))
+    ) : (
+        <>
+            <div className="d-flex justify-content-center mt-4 mb-4">
+                <h4>Lo sentimos pero no tenemos coches disponibles para el <br />
+                    periodo seleccionado, prueba a elegir otras fechas.</h4>
+            </div>
+            <hr />
+            <div className="d-flex justify-content-center">
+                <Button
+                    type="button"
+                    style={{
+                        color: "black",
+                        padding: "7px",
+                        width: '30%',
+                        backgroundColor: "beige",
+                        border: "solid BurlyWood 2px"
+                    }}
+                    onClick={navegarListado}
+                    texto="Modificar búsqueda"
+                />
+            </div>
+            <hr />
+        </>
+    )
+    }
+</div>
         </div>
-
     )
 
 }
--- src/componentes/Fleet/Fleet.tsx
+++ src/componentes/Fleet/Fleet.tsx
@@ -3,6 +3,7 @@ import CarDetails from './CarDetails';
 import { Car } from '../../types/types';
 import FleetFilter from '../Fleet/FleetFilter';
 import { useSearchParams } from 'react-router-dom';
+import SkeletonCard from '../Elements/SkeletonCard';
 
 const Fleet = () => {
 
@@ -14,19 +15,22 @@ const Fleet = () => {
     const [selectedSucursalId, setSelectedSucursalId] = useState<number>(0);
 
     const [searchParams] = useSearchParams();
-      const fechaInicio = searchParams.get('fechainicio') ? new Date(searchParams.get('fechainicio')!) : new Date();
-      const fechaFin = searchParams.get('fechaFin') ? new Date(searchParams.get('fechaFin')!) : new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
-      const horarioRecogida = searchParams.get('horarioRecogida') || '10:00';
-      const horarioDevolucion = searchParams.get('horarioDevolucion') || '12:00';
-      const sucursalId = Number(searchParams.get('sucursalId'));
-      const sucursalDevolucion = Number(searchParams.get('sucursalDevolucion'));
-    
+    const fechaInicio = searchParams.get('fechainicio') ? new Date(searchParams.get('fechainicio')!) : new Date();
+    const fechaFin = searchParams.get('fechaFin') ? new Date(searchParams.get('fechaFin')!) : new Date(Date.now() + 2 * 24 * 60 * 60 * 1000);
+    const horarioRecogida = searchParams.get('horarioRecogida') || '10:00';
+    const horarioDevolucion = searchParams.get('horarioDevolucion') || '12:00';
+    const sucursalId = Number(searchParams.get('sucursalId'));
+    const sucursalDevolucion = Number(searchParams.get('sucursalDevolucion'));
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
+        setIsLoading(true);
         fetch('http://localhost:5038/api/coche/con-grupo') // si pruebo con el endpoint que trae grupos (/con-grupo) me muestra los grupos pero los creados por form no
             .then(res => res.json())
             .then(data => {
                 console.log(data);
                 setCoches(data.$values);
+                setIsLoading(false);
             })
             .catch(error => console.error('Error: ', error));
     }, []); // este array vacío evita llamadas infinitas a la api
@@ -44,33 +48,44 @@ const Fleet = () => {
         <div className='container'>
             <h2> Flota de coches </h2>
 
-            <FleetFilter 
+            <FleetFilter
                 setSelectedPlazas={setSelectedPlazas}
                 setSelectedTipoCambio={setSelectedTipoCambio}
                 setSelectedTipoCoche={setSelectedTipoCoche}
                 setSelectedSucursalId={setSelectedSucursalId}
-             />
-            
+            />
+
             <div className='row'>
-                {cochesFiltrados.length > 0 ? (
+                {isLoading ? (
+                    <>
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                    </>
+                ) : cochesFiltrados.length > 0 ? (
                     cochesFiltrados.map((coche) => (
-                        <div className="col-12 col-md-6 col-lg-4 mb-3" 
+                        <div className="col-12 col-md-6 col-lg-4 mb-3"
                             key={coche.id}
                         >
-                            <CarDetails 
+                            <CarDetails
                                 coche={coche}
                                 selectedSucursalId={sucursalId}
                                 fechainicio={fechaInicio}
                                 fechaFin={fechaFin}
                                 selectedHorarioRecogida={horarioRecogida}
                                 selectedHorarioDevolucion={horarioDevolucion}
-                                sucursalDevolucion={sucursalDevolucion}                            
+                                sucursalDevolucion={sucursalDevolucion}
                             />
                         </div>
                     ))
                 ) : (
                     <p>Ningún coche coincide con su selección</p>
-                )}
+                )
+                }
+
 
             </div>
         </div>
